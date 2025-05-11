5e295456ddf50f2e12c8fbbb475e860a4181c8b1
jmerort <jmerortz@gmail.com>
2025-05-11 09:48:43 +0200

    Finished excercise 1 from chapter 7

---
+++ CH07/7_1.py
@@ -0,0 +1,12 @@
+"""
+7-1
+Rental Car: Write a program that asks the user what kind of rental car they
+would like. Print a message about that car, such as “Let me see if I can find you
+a Subaru.”
+
+May 2025
+@jmerort
+"""
+
+car = input ("Which car are you looking for?\nBrand: ")
+print (f"Here are all the {car.title()} models we have...")
