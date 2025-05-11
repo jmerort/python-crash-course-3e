47c6a75d51d7ede69a90804bf78e2411aec64ea0
jmerort <jmerortz@gmail.com>
2025-05-11 16:09:06 +0200

    Finished excercise 4 from chapter 7

--- CH07/7_4.py
+++ CH07/7_4.py
@@ -1,1 +1,20 @@
-# hola
+"""
+7-4
+Pizza Toppings: Write a loop that prompts the user to enter a series of
+pizza toppings until they enter a 'quit' value. As they enter each topping, print
+a message saying you'll add that topping to their pizza.
+
+May 2025
+@jmerort
+"""
+
+topping = ''
+
+while True:
+    topping = input("Enter a topping or enter 'quit' to continue: ")
+    if topping == 'quit':
+        print("Done. Enjoy your pizza!")
+        break
+    else:
+        print(f"{topping.title()} added to your pizza.")
+    
