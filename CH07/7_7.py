2673b12e12a894dc10d154c18989b60d71baa1a3
jmerort <jmerortz@gmail.com>
2025-05-17 20:14:21 +0200

    Finished excercise 7 from chapter 7

---
+++ CH07/7_7.py
@@ -0,0 +1,11 @@
+"""
+7-7
+Infinity: Write a loop that never ends, and run it. (To end the loop, press
+CTRL-C or close the window displaying the output.)
+
+May 2025
+@jmerort
+"""
+
+while True:
+    print("Infinite Loop... Press Ctrl-C to exit...")
