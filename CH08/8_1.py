222449b0f3d0f353789f9159ce2c46f1cec77c06
jmerort <jmerortz@gmail.com>
2025-06-15 11:02:39 +0200

    Finished excercise 1 of chapter 8

---
+++ CH08/8_1.py
@@ -0,0 +1,18 @@
+"""
+8-1
+Message: Write a function called display_message() that prints one sen-
+tence telling everyone what you are learning about in this chapter. Call the
+function, and make sure the message displays correctly.
+
+Jun 2025
+@jmerort
+"""
+
+def display_message():
+    """
+    Prints what i'm learning in this chapter.
+    """
+    print("In chapter 8 of the Python Crash Course, I've learned so far what a function is, "
+    "what its parts are and how to call it.")
+
+display_message()
